a8a5e813af690a22ddd1bb72f07e63dfcff1ebe7
chaplin2007 <41979802+chaplin2007@users.noreply.github.com>
2018-08-01 15:22:35 +0900

    Add files via upload

---
+++ src/main/java/com/github/jitpack/App2.java
@@ -0,0 +1,72 @@
+package com.github.jitpack;
+
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App2
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+
+
+
+
+
+public static void deleteAllFiles(String path){
+		
+		File file = new File(path);
+		//폴더내 파일을 배열로 가져온다.
+		File[] tempFile = file.listFiles();
+
+		if(tempFile.length >0){
+			
+			for (int i = 0; i < tempFile.length; i++) {
+				
+				if(tempFile[i].isFile()){
+					tempFile[i].delete();
+				}else{
+					//재귀함수
+					deleteAllFiles(tempFile[i].getPath());
+				}
+				tempFile[i].delete();
+				
+			}
+			file.delete();
+			
+		}
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
