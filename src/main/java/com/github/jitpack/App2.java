5e6342209b947501bf87c5e8596bae82dac274c9
chaplin2007 <41979802+chaplin2007@users.noreply.github.com>
2018-08-01 15:25:06 +0900

    Update App2.java

--- src/main/java/com/github/jitpack/App2.java
+++ src/main/java/com/github/jitpack/App2.java
@@ -24,26 +24,7 @@ public class App2
 
 public static void deleteAllFiles(String path){
 		
-		File file = new File(path);
-		
-		File[] tempFile = file.listFiles();
-
-		if(tempFile.length >0){
-			
-			for (int i = 0; i < tempFile.length; i++) {
-				
-				if(tempFile[i].isFile()){
-					tempFile[i].delete();
-				}else{
-				
-					deleteAllFiles(tempFile[i].getPath());
-				}
-				tempFile[i].delete();
-				
-			}
-			file.delete();
-			
-		}
+		System.out.println("hello World kkkk");
 		
 	}
 
